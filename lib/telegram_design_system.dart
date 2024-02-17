c6e2e47d9615fcb05eb30aaa5c9e15e6f8aa3743
Mo Elrayes <MSalahElrayes@gmail.com>
2024-02-17 15:05:23 +0300

    Add class Colors for app

--- lib/telegram_design_system.dart
+++ lib/telegram_design_system.dart
@@ -1,7 +1,2 @@
 library telegram_design_system;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
---
+++ lib/theme/data/colors.dart
@@ -0,0 +1,340 @@
+import 'package:flutter/material.dart';
+
+class GrayDark extends Color {
+  const GrayDark(super.value);
+
+  static const grayDark1 = GrayDark(0xFFB7B7B7);
+  static const grayDark2 = GrayDark(0xFFA8A8A8);
+  static const grayDark3 = GrayDark(0xFFA7A7A7);
+  static const grayDark4 = GrayDark(0xFFA4A4A4);
+  static const grayDark5 = GrayDark(0xFF9B9B9B);
+  static const grayDark6 = GrayDark(0xFF999999);
+  static const grayDark7 = GrayDark(0xFF949494);
+  static const grayDark8 = GrayDark(0xFF888888);
+  static const grayDark9 = GrayDark(0xFF868686);
+  static const grayDark10 = GrayDark(0xFF757575);
+  static const grayDark11 = GrayDark(0xFF555555);
+  static const grayDark12 = GrayDark(0xFF393939);
+  static const grayDark13 = GrayDark(0xFF333333);
+  static const grayDark14 = GrayDark(0xFF232323);
+  static const grayDark15 = GrayDark(0xFF212121);
+  static const grayDark16 = GrayDark(0xFF202020);
+
+  static const grayAlpha80 = GrayDark(0xFFCC444444);
+}
+
+class GrayLight extends Color {
+  const GrayLight(super.value);
+
+  static const grayLight1 = GrayLight(0xFFFAFAFA);
+  static const grayLight2 = GrayLight(0xFFF9F9F9);
+  static const grayLight3 = GrayLight(0xFFF7F7F7);
+  static const grayLight4 = GrayLight(0xFFF0F0F0);
+  static const grayLight5 = GrayLight(0xFFEEEEEE);
+  static const grayLight6 = GrayLight(0xFFEDEDED);
+  static const grayLight7 = GrayLight(0xFFEBEBEB);
+  static const grayLight8 = GrayLight(0xFFE7E7E7);
+  static const grayLight9 = GrayLight(0xFFDFDFDF);
+  static const grayLight10 = GrayLight(0xFFD9D9D9);
+}
+
+class White extends Color {
+  const White(super.value);
+
+  static const white1 = White(0xFFFFFFFF);
+  static const whiteAlpha80 = White(0xFFCCFFFFFF);
+  static const whiteAlpha40 = White(0xFF66FFFFFF);
+  static const whiteAlpha35 = White(0xFF59FFFFFF);
+  static const whiteAlpha25 = White(0xFF40FFFFFF);
+  static const whiteAlpha1 = White(0xFF03FFFFFF);
+}
+
+class Black extends Color {
+  const Black(super.value);
+
+  static const black1 = Black(0xFF000000);
+  static const blackAlpha75 = Black(0xFFBF000000);
+  static const blackAlpha60 = Black(0xFF99000000);
+  static const blackAlpha40 = Black(0xFF66000000);
+  static const blackAlpha35 = Black(0xFF59000000);
+  static const blackAlpha30 = Black(0xFF4D000000);
+  static const blackAlpha25 = Black(0xFF40000000);
+  static const blackAlpha20 = Black(0xFF33000000);
+  static const blackAlpha17 = Black(0xFF2B000000);
+  static const blackAlpha15 = Black(0xFF26000000);
+  static const blackAlpha14 = Black(0xFF24000000);
+  static const blackAlpha13 = Black(0xFF21000000);
+  static const blackAlpha12 = Black(0xFF1F000000);
+  static const blackAlpha10 = Black(0xFF1A000000);
+  static const blackAlpha5 = Black(0xFF0D000000);
+  static const blackAlpha3 = Black(0xFF08000000);
+}
+
+class Red extends Color {
+  const Red(super.value);
+
+  static const red1 = Red(0xFFF57F7F);
+  static const red2 = Red(0xFFE14D4D);
+  static const red3 = Red(0xFFDE3A3A);
+  static const red4 = Red(0xFFCE494A);
+  static const red5 = Red(0xFF853D40);
+}
+
+class Yellow extends Color {
+  const Yellow() : super(0xFFF3C04B);
+}
+
+class BlueLight extends Color {
+  const BlueLight(super.value);
+
+  static const blueLight1 = BlueLight(0xFF84CAFE);
+  static const blueLight2 = BlueLight(0xFF61C5F9);
+  static const blueLight3 = BlueLight(0xFF50B7FC);
+  static const blueLight4 = BlueLight(0xFF4CB3F5);
+  static const blueLight5 = BlueLight(0xFF49ACEA);
+  static const blueLight6 = BlueLight(0xFF3BA9EC);
+  static const blueLight7 = BlueLight(0xFF32A8E6);
+  static const blueLight8 = BlueLight(0xFF19A7E7);
+}
+
+class BlueSemiLight extends Color {
+  const BlueSemiLight(super.value);
+
+  static const blueSemiLight1 = BlueSemiLight(0xFF84CAFE);
+  static const blueSemiLight2 = BlueSemiLight(0xFF62B4EE);
+  static const blueSemiLight3 = BlueSemiLight(0xFF62B0EB);
+  static const blueSemiLight4 = BlueSemiLight(0xFF5FAEE7);
+  static const blueSemiLight5 = BlueSemiLight(0xFF55AAEB);
+  static const blueSemiLight6 = BlueSemiLight(0xFF53ADE9);
+  static const blueSemiLight7 = BlueSemiLight(0xFF52ABEF);
+  static const blueSemiLight8 = BlueSemiLight(0xFF51AEE7);
+  static const blueSemiLight9 = BlueSemiLight(0xFF50A7EA);
+  static const blueSemiLight10 = BlueSemiLight(0xFF4FA6E9);
+  static const blueSemiLight11 = BlueSemiLight(0xFF2E96DF);
+  static const blueSemiLight12 = BlueSemiLight(0xFF1C93E3);
+}
+
+class BlueDark extends Color {
+  const BlueDark(super.value);
+
+  static const blueDark1 = BlueDark(0xFF6C9FD2);
+  static const blueDark2 = BlueDark(0xFF629AC9);
+  static const blueDark3 = BlueDark(0xFF5A8EBD);
+  static const blueDark4 = BlueDark(0xFF517DA2);
+  static const blueDark5 = BlueDark(0xFF427DA9);
+  static const blueDark6 = BlueDark(0xFF1B5A7D);
+}
+
+class Blurple extends Color {
+  const Blurple(super.value);
+
+  static const blueBlurple1 = Blurple(0xFF6C9FD2);
+  static const blueBlurple2 = Blurple(0xFF539EF9);
+  static const blueBlurple3 = Blurple(0xFF70AAEA);
+}
+
+class GreenLight extends Color {
+  const GreenLight(super.value);
+
+  static const greenLight1 = GreenLight(0xFFEFFEDD);
+  static const greenLight2 = GreenLight(0xFFEAF7ED);
+}
+
+class GreenMedium extends Color {
+  const GreenMedium(super.value);
+
+  static const greenMedium1 = GreenMedium(0xFF71E079);
+  static const greenMedium2 = GreenMedium(0xFF4ECC5E);
+  static const greenMedium3 = GreenMedium(0xFF60C255);
+  static const greenMedium4 = GreenMedium(0xFF62AC55);
+}
+
+class GreenBright extends Color {
+  const GreenBright(super.value);
+
+  static const greenBright1 = GreenBright(0xFF5ACD30);
+  static const greenBright2 = GreenBright(0xFF47A938);
+  static const greenBright3 = GreenBright(0xFF04A410);
+}
+
+class SteelGrayBright extends Color {
+  const SteelGrayBright(super.value);
+
+  static const steelGrayBright1 = SteelGrayBright(0xFFDEECEF);
+  static const steelGrayBright2 = SteelGrayBright(0xFFD5E8F7);
+  static const steelGrayBright3 = SteelGrayBright(0xFFD2E9FB);
+  static const steelGrayBright4 = SteelGrayBright(0xFFC5DBF0);
+
+  static const steelGrayAlpha50 = SteelGrayBright(0xFF806C9FD2);
+}
+
+class SteelGrayLight extends Color {
+  const SteelGrayLight(super.value);
+
+  static const steelGrayLight1 = SteelGrayLight(0xFFEDEEF0);
+  static const steelGrayLight2 = SteelGrayLight(0xFFEDEEF2);
+  static const steelGrayLight3 = SteelGrayLight(0xFFE1E4E9);
+  static const steelGrayLight4 = SteelGrayLight(0xFFC9CCD3);
+  static const steelGrayLight5 = SteelGrayLight(0xFFC6CACD);
+  static const steelGrayLight6 = SteelGrayLight(0xFFC5C9CC);
+  static const steelGrayLight7 = SteelGrayLight(0xFFBDC2C5);
+  static const steelGrayLight8 = SteelGrayLight(0xFFB0B9C2);
+  static const steelGrayLight9 = SteelGrayLight(0xFFB0B5B9);
+  static const steelGrayLight10 = SteelGrayLight(0xFFADB6BD);
+  static const steelGrayLight11 = SteelGrayLight(0xFFA3ACB3);
+  static const steelGrayLight12 = SteelGrayLight(0xFFA2A9B1);
+  static const steelGrayLight13 = SteelGrayLight(0xFFA1AAB3);
+  static const steelGrayLight14 = SteelGrayLight(0xFF9DA4AC);
+  static const steelGrayLight15 = SteelGrayLight(0xFF97A0A7);
+  static const steelGrayAlpha40 = SteelGrayLight(0xFF66728391);
+}
+
+class SteelGrayDark extends Color {
+  const SteelGrayDark(super.value);
+
+  static const steelGrayLight1 = SteelGrayDark(0xFF95999C);
+  static const steelGrayDark2 = SteelGrayDark(0xFF90979D);
+  static const steelGrayDark3 = SteelGrayDark(0xFF8E959B);
+  static const steelGrayDark4 = SteelGrayDark(0xFF8E9397);
+  static const steelGrayDark5 = SteelGrayDark(0xFF8D9296);
+  static const steelGrayDark6 = SteelGrayDark(0xFF8E9196);
+  static const steelGrayDark7 = SteelGrayDark(0xFF8D8E90);
+  static const steelGrayDark8 = SteelGrayDark(0xFF889198);
+  static const steelGrayDark9 = SteelGrayDark(0xFF879097);
+  static const steelGrayDark10 = SteelGrayDark(0xFF878C90);
+  static const steelGrayDark11 = SteelGrayDark(0xFF848D94);
+  static const steelGrayDark12 = SteelGrayDark(0xFF848B93);
+  static const steelGrayDark13 = SteelGrayDark(0xFF7D848A);
+  static const steelGrayDark14 = SteelGrayDark(0xFF6B7278);
+  static const steelGrayDark15 = SteelGrayDark(0xFF676A6F);
+}
+
+class BlueToPurple extends LinearGradient {
+  const BlueToPurple({
+    super.colors = const [
+      Color(0xFF648BF4),
+      Color(0xFFD2597A),
+    ],
+    super.begin = Alignment.topLeft,
+    super.end = Alignment.bottomRight,
+    super.stops,
+    super.tileMode,
+  });
+}
+
+class VoipOverlay extends LinearGradient {
+  const VoipOverlay({
+    super.colors = const [
+      Color(0xFF2C4D6B),
+      Color(0xFF236D92),
+    ],
+    super.begin = Alignment.bottomLeft,
+    super.end = Alignment.topRight,
+    super.stops,
+    super.tileMode,
+  });
+}
+
+class VoipMuted extends LinearGradient {
+  const VoipMuted({
+    super.colors = const [
+      Color(0xFF539EF9),
+      Color(0xFF61C5F9),
+    ],
+    super.begin = Alignment.topCenter,
+    super.end = Alignment.bottomCenter,
+    super.stops,
+    super.tileMode,
+  });
+}
+
+class VoipMutedAlpha50 extends LinearGradient {
+  const VoipMutedAlpha50({
+    super.colors = const [
+      Color(0xFF539EF9),
+      Color(0xFF61C5F9),
+    ],
+    super.begin = Alignment.topCenter,
+    super.end = Alignment.bottomCenter,
+    super.stops,
+    super.tileMode,
+  });
+}
+
+class VoipMutedAlpha10 extends LinearGradient {
+  const VoipMutedAlpha10({
+    super.colors = const [
+      Color(0xFF539EF9),
+      Color(0xFF61C5F9),
+    ],
+    super.begin = Alignment.topCenter,
+    super.end = Alignment.bottomCenter,
+    super.stops,
+    super.tileMode,
+  });
+}
+
+class VoipSpeaking extends LinearGradient {
+  const VoipSpeaking({
+    super.colors = const [
+      Color(0xFF62D2C1),
+      Color(0xFF71E079),
+    ],
+    super.begin = Alignment.topCenter,
+    super.end = Alignment.bottomCenter,
+    super.stops,
+    super.tileMode,
+  });
+}
+
+class VoipSpeakingAlpha50 extends LinearGradient {
+  const VoipSpeakingAlpha50({
+    super.colors = const [
+      Color(0xFF62D2C1),
+      Color(0xFF71E079),
+    ],
+    super.begin = Alignment.topCenter,
+    super.end = Alignment.bottomCenter,
+    super.stops,
+    super.tileMode,
+  });
+}
+
+class VoipSpeakingAlpha40 extends LinearGradient {
+  const VoipSpeakingAlpha40({
+    super.colors = const [
+      Color(0xFF62D2C1),
+      Color(0xFF71E079),
+    ],
+    super.begin = Alignment.topCenter,
+    super.end = Alignment.bottomCenter,
+    super.stops,
+    super.tileMode,
+  });
+}
+
+class VoipApeakingAlpha10 extends LinearGradient {
+  const VoipApeakingAlpha10({
+    super.colors = const [
+      Color(0xFF62D2C1),
+      Color(0xFF71E079),
+    ],
+    super.begin = Alignment.topCenter,
+    super.end = Alignment.bottomCenter,
+    super.stops,
+    super.tileMode,
+  });
+}
+
+class GreenGradient extends LinearGradient {
+  const GreenGradient({
+    super.colors = const [
+      Color(0xFF48CA6A),
+      Color(0xFF13B8AA),
+    ],
+    super.begin = Alignment.centerLeft,
+    super.end = Alignment.centerRight,
+    super.stops,
+    super.tileMode,
+  });
+}
